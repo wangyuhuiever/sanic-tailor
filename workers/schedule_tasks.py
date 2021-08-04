9fadcee1e13ea50d4e6468471a81cb5f762fb42a
Yuhui <wangyuhui@sf-pharma.com>
2021-08-04 17:44:09 +0800

    [IMP]schedule add async

--- workers/schedule_tasks.py
+++ workers/schedule_tasks.py
@@ -1,7 +1,16 @@
 from workers.celery_client import app
+from utils.database import Database
+import asyncio
 import logging
 
 _logger = logging.getLogger(__name__)
+
+
+async def insert_db():
+    sql = "insert into test_table (col1, col2) values ($1, $2) returning id;"
+    values = [1, 'test']
+    await Database().execute(sql, *values)
+    return
 
 
 @app.task(name='schedule_task')
@@ -10,6 +19,8 @@ def schedule_task(*args, **kwargs):
         'args': args,
         'kwargs': kwargs
     })
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(insert_db())
     return
 
 
