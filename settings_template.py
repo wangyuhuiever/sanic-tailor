8fc005401dcf3fb3b027db286980e796bf0d9c42
Yuhui <wangyuhui@sf-pharma.com>
2021-08-06 15:46:31 +0800

    [IMP] improve settings_template.py

--- settings_template.py
+++ settings_template.py
@@ -3,18 +3,26 @@
 class Sanic:
     name = "Yuhui Sanic"
 
+
+class Database:
+    start = True
+
     DB_HOST = 'localhost'
     DB_PORT = '5432'
     DB_USER = ''
     DB_PASS = ''
     DB_NAME = ''
 
+
+class RabbitMQ:
     MQ_HOST = ''
     MQ_PORT = ''
     MQ_USER = ''
     MQ_PASS = ''
     MQ_VHOST = ''
 
+
+class Redis:
     REDIS_HOST = ''
     REDIS_PORT = ''
     REDIS_PASS = ''
@@ -23,19 +31,20 @@ class Sanic:
 
 class Celery:
     name = "Yuhui Sanic Celery"
+    start = True
 
-    custom_db_host = Sanic.DB_HOST
-    custom_db_port = Sanic.DB_PORT
-    custom_db_user = Sanic.DB_USER
-    custom_db_pass = Sanic.DB_PASS
-    custom_db_name = Sanic.DB_NAME
+    custom_db_host = Database.DB_HOST
+    custom_db_port = Database.DB_PORT
+    custom_db_user = Database.DB_USER
+    custom_db_pass = Database.DB_PASS
+    custom_db_name = Database.DB_NAME
 
     custom_flower_port = ''
     custom_flower_user = ''
     custom_flower_pass = ''
 
-    broker_url = f'amqp://{Sanic.MQ_USER}:{Sanic.MQ_PASS}@{Sanic.MQ_HOST}:{Sanic.MQ_PORT}/{Sanic.MQ_VHOST}'
-    result_backend = f'redis://default:{Sanic.REDIS_PASS}@{Sanic.REDIS_HOST}:{Sanic.REDIS_PORT}/{Sanic.REDIS_DB}'
+    broker_url = f'amqp://{RabbitMQ.MQ_USER}:{RabbitMQ.MQ_PASS}@{RabbitMQ.MQ_HOST}:{RabbitMQ.MQ_PORT}/{RabbitMQ.MQ_VHOST}'
+    result_backend = f'redis://default:{Redis.REDIS_PASS}@{Redis.REDIS_HOST}:{Redis.REDIS_PORT}/{Redis.REDIS_DB}'
     result_expires = 3600
     enable_utc = True
     beat_schedule = {
