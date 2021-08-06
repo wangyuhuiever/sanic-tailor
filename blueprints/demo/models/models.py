d5f7c912f6761e275924b63f1ab1a6fc585b120a
Yuhui <wangyuhui@sf-pharma.com>
2021-08-06 18:33:22 +0800

    [IMP] add httpx

--- blueprints/demo/models/models.py
+++ blueprints/demo/models/models.py
@@ -1,10 +1,17 @@
 from utils.db import Database
+import httpx
 
 
 class DemoModel(Database):
 
     async def insert_data(self):
+        async with httpx.AsyncClient() as client:
+            headers = {}
+            payload = {}
+            response = await client.get("http://www.baidu.com", headers=headers)
+
+        r = response.text
         sql = "insert into test_table (col1, col2) values ($1, $2) returning id;"
-        values = [1, 'test']
+        values = [1, r]
         res = await self.execute(sql, *values)
         return res
