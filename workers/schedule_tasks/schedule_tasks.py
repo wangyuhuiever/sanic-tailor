ee4734ab17337f94a1a8aa14b8c8d630e5d78d97
Yuhui <wangyuhui@sf-pharma.com>
2021-08-06 17:13:07 +0800

    [IMP] improve template

--- workers/schedule_tasks/schedule_tasks.py
+++ workers/schedule_tasks/schedule_tasks.py
@@ -10,8 +10,18 @@ async def insert_db():
     sql = "insert into test_table (col1, col2) values ($1, $2) returning id;"
     values = [1, 'test']
     res = await database.execute(sql, *values)
-    print(res)
-    return
+    data = [dict(r) for r in res]
+    _logger.info({'insert_db': data})
+    return data
+
+
+async def select_db(res_id):
+    sql = "select * from test_table where id = $1;"
+    values = [res_id]
+    res = await database.execute(sql, *values)
+    data = [dict(r) for r in res]
+    _logger.info({'select_db': data})
+    return data
 
 
 @app.task(name='schedule_task')
@@ -21,7 +31,11 @@ def schedule_task(*args, **kwargs):
         'kwargs': kwargs
     })
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(insert_db())
+    res_id = loop.run_until_complete(insert_db())
+    _logger.info({'schedule_task id': res_id})
+    res_id = res_id[0].get('id')
+    res = loop.run_until_complete(select_db(res_id))
+    _logger.info({'schedule_task res': res})
     return
 
 
