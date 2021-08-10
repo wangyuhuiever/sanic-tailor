8500a50c091e2117b343b78764cbfa81f433ced7
Yuhui <wangyuhui@sf-pharma.com>
2021-08-10 10:32:44 +0800

    [IMP] improve cron job

--- workers/schedule_tasks/schedule_tasks.py
+++ workers/schedule_tasks/schedule_tasks.py
@@ -1,9 +1,23 @@
 from workers import app
-from workers.utils.database import database
+from workers.utils.db import database
+from celery.schedules import crontab
 import asyncio
 import logging
 
 _logger = logging.getLogger(__name__)
+
+
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+
+    # 每10秒触发一次
+    sender.add_periodic_task(10, schedule_task.s('args: ', '1', '2', kwarg='kwarg', kwarg2='kwarg2'))
+
+    # 每天10点30触发一次
+    sender.add_periodic_task(
+        crontab(hour=10, minute=30),
+        cron_task.s('cron args:', '1', '2', kwarg='kwarg')
+    )
 
 
 async def insert_db():
@@ -39,4 +53,10 @@ def schedule_task(*args, **kwargs):
     return
 
 
-
+@app.task(name='cron_task')
+def cron_task(*args, **kwargs):
+    _logger.info({
+        'args': args,
+        'kwargs': kwargs
+    })
+    return
