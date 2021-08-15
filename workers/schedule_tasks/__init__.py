d66e68ddb9bc8b0246a906d9440a58222d36f413
Yuhui <wangyuhuiyi@gmail.com>
2021-08-15 10:45:26 +0800

    [IMP]improve model name

--- workers/schedule_tasks/__init__.py
+++ workers/schedule_tasks/__init__.py
@@ -1,1 +1,17 @@
-from . import schedule_tasks
+from workers import app
+from .models import schedule_task, cron_task
+from celery.schedules import crontab
+
+
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+
+    # 每10秒触发一次
+    sender.add_periodic_task(10, schedule_task.s('args: ', '1', '2', kwarg='kwarg', kwarg2='kwarg2'))
+
+    # 每天10点30触发一次
+    sender.add_periodic_task(
+        crontab(hour=10, minute=30),
+        cron_task.s('cron args:', '1', '2', kwarg='kwarg')
+    )
+
--- workers/schedule_tasks/models.py
+++ workers/schedule_tasks/models.py
@@ -1,23 +1,9 @@
 from workers import app
 from workers.utils.db import database
-from celery.schedules import crontab
 import asyncio
 import logging
 
 _logger = logging.getLogger(__name__)
-
-
-@app.on_after_finalize.connect
-def setup_periodic_tasks(sender, **kwargs):
-
-    # 每10秒触发一次
-    sender.add_periodic_task(10, schedule_task.s('args: ', '1', '2', kwarg='kwarg', kwarg2='kwarg2'))
-
-    # 每天10点30触发一次
-    sender.add_periodic_task(
-        crontab(hour=10, minute=30),
-        cron_task.s('cron args:', '1', '2', kwarg='kwarg')
-    )
 
 
 async def insert_db():
