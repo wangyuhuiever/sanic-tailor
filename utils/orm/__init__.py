21bb4e3bc185dc57b4c6fccc50981a14ced4d8e5
Yuhui <wangyuhui@sf-pharma.com>
2021-08-18 14:59:52 +0800

    [IMP]improve tortoise orm init

--- utils/orm/__init__.py
+++ utils/orm/__init__.py
@@ -1,10 +1,11 @@
 #! -*- coding: utf-8 -*-
 from settings import ORM
-from . import tortoise_orm
 
 
 def init_orm(app):
 
     if ORM.TortoiseORM._start:
+        from . import tortoise_orm
+
         tortoise_config = dict(ORM.TortoiseORM())
         tortoise_orm.init_orm(app, tortoise_config)
