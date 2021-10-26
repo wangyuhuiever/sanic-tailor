c5553ec22168a0e2a86f4a1be3f5d5d5606d7e38
Yuhui <>
2021-10-26 17:31:04 +0800

    [ADD]add cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sanic import Sanic
+from sanic_cors import CORS
 from addons import api
 import settings
 from utils import init_utils
@@ -7,7 +8,8 @@ app = Sanic(settings.Sanic.name)
 app.update_config(settings.Sanic)
 app.blueprint(api)
 
+CORS(app)
 init_utils(app)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, workers=8, access_log=True, auto_reload=False)
+    app.run(host="0.0.0.0", port=5050, workers=8, access_log=True, auto_reload=False)
